294e3d630450f594386caf9663af7db2a1e97190
thearnabsaha <thearnabsaha201@gmail.com>
2022-07-13 19:55:57 +0530

    s1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+import Search from './search.svg'
+import Card from './Components/Card'
+import ("./App.css")
+const API_URL= "http://www.omdbapi.com/?apikey=22b16ba0";
 
-function App() {
+const App = () => {
+  const [movie, setMovie] = useState([])
+  const [searchTerm, setSearchTerm] = useState("")
+  const fetchMovies=async(title)=>{
+    const res= await fetch(`${API_URL}&s=${title}`)
+    const data = await res.json()
+    setMovie(data.Search)
+    console.log(movie);
+  }
+  const handleEnter=(e)=>{
+    if(e.key==="Enter"){
+      fetchMovies(searchTerm)
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="app">
+        <h1>TAS Movies</h1>
+        <input type="text" value={searchTerm} onChange={(e)=>{setSearchTerm(e.target.value)}} placeholder="Search For Movies" className="search" onKeyUp={handleEnter}/>
+        <img src={Search} alt="search" onClick={()=>{fetchMovies(searchTerm)}}/>
+        {movie?.length>0?(
+          <div className="container">
+            {
+              movie.map((e)=>{
+                return(
+                  <div>
+                    <Card data={e}/>
+                  </div>
+                )
+              })
+            }
+          </div>
+        ):(
+          <div className="empty">
+            <h2>No movies found</h2>
+          </div>
+        )}
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
